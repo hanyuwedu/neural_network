d1e35e156f50c709426f007838c18fd9e0a93c55
hanyuw.edu <hanyuw.edu@gmail.com>
2024-07-10 14:38:00 +0800

    Address tests to all projects

---
+++ tests/__init__.py
@@ -0,0 +1,4 @@
+from .logistic_regression_with_a_neural_network_mindset_tests import *
+from .planar_data_classification_with_one_hidden_layer_tests import *
+from. building_your_deep_neural_network_step_by_step_tests import *
+from .deep_neural_network_application_tests import *
